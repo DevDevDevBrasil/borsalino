37a4c14c7cb09c673f37bc89d89bfa36c2e6ab01
DevDevDevBrasil <54514525+DevDevDevBrasil@users.noreply.github.com>
2019-08-25 19:29:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,3 @@ for (site of siteExceptions) {
 if (count === 0) {
     document.body.innerHTML = document.body.innerHTML.replace(/Jair Messias Bolsonaro/gi, 'Mijair Messias Borsalino').replace(/Jair Bolsonaro/gi, 'Mijair Borsalino').replace(/Bolsonaro/gi, 'Borsalino');
 }
-
-// if (window.location.href.indexOf("twitter") === -1 && window.location.href.indexOf("linkedin.com") === -1 && window.location.href.indexOf("facebook") === -1) {
-//     document.body.innerHTML = document.body.innerHTML.replace(/Jair Bolsonaro/gi, 'Mijair Borsalino').replace(/Bolsonaro/gi, 'Borsalino');
-// }
